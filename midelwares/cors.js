6acbc1e83b10a809670a3c4b000962926c6a6a58
Gabriel Teresczuk <gabrielteresczuk@gmail.com>
2022-12-28 10:02:48 -0300

    1.0.3

--- midelwares/cors.js
+++ midelwares/cors.js
@@ -1,5 +1,5 @@
 const cors = function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
